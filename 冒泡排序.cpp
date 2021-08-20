b4fa9f73f3bc2242ad65d8db71abef48564e415b
cmathFPV <84088992+gxq-is-a-lovely-girl@users.noreply.github.com>
2021-08-20 17:02:29 +0800

    Update 冒泡排序.cpp

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -1,6 +1,6 @@
-void Bubble sorting(){
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n-1-i;j++){
+void Bubble_sorting(){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-1-i;j++){
 			if(a[j]>a[j+1]){
 				swap(a[j],a[j+1]);
 			}
